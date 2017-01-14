45d56d9afdc8c4486ca8cddc154ad9f3f1b3a4f2
grimergrim <iavorskyi.v.v@gmail.com>
2017-01-14 11:55:06 +0200

    Add. User profile, view pager for car photos.

--- app/src/main/java/ru/nadocars/messanger/ui/profile/ProfileActivity.java
+++ app/src/main/java/ru/nadocars/messanger/ui/profile/ProfileActivity.java
@@ -1,6 +1,11 @@
 package ru.nadocars.messanger.ui.profile;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import ru.nadocars.messanger.R;
@@ -11,6 +16,8 @@ public class ProfileActivity extends AppCompatActivity implements ProfileView {
 
     ProfilePresenter mProfilePresenter;
     Navigator mNavigator;
+    private ViewPager mViewPager;
+    private PagerAdapter mPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,10 @@ public class ProfileActivity extends AppCompatActivity implements ProfileView {
         mProfilePresenter = ProfilePresenterImpl.getPreLoginPresenter();
         mProfilePresenter.setView(this);
         mNavigator = new NavigatorImpl();
+
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager(), 3);
+        mViewPager.setAdapter(mPagerAdapter);
     }
 
     @Override
@@ -26,4 +37,34 @@ public class ProfileActivity extends AppCompatActivity implements ProfileView {
         super.onPause();
         mProfilePresenter.setView(null);
     }
+
+    @Override
+    public void onBackPressed() {
+        if (mViewPager.getCurrentItem() == 0) {
+            super.onBackPressed();
+        } else {
+            mViewPager.setCurrentItem(mViewPager.getCurrentItem() - 1);
+        }
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+
+        private int mNumberOfPages;
+
+        public ScreenSlidePagerAdapter(FragmentManager fragmentManager, int numberOfPages) {
+            super(fragmentManager);
+            mNumberOfPages = numberOfPages;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return new ScreenSlidePageFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return mNumberOfPages;
+        }
+    }
+
 }
---
+++ app/src/main/java/ru/nadocars/messanger/ui/profile/ScreenSlidePageFragment.java
@@ -0,0 +1,19 @@
+package ru.nadocars.messanger.ui.profile;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ru.nadocars.messanger.R;
+
+public class ScreenSlidePageFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_screen_slide_page, container, false);
+        return rootView;
+    }
+
+}
